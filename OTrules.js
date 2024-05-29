faa0e17d76da107a20867533dbc6bd90c37c8698
rmatarazzo <168002928+rmatarazzo@users.noreply.github.com>
2024-05-29 12:55:34 -0400

    edit to project3

--- OTrules.js
+++ OTrules.js
@@ -1,18 +1,17 @@
 graph TD
-    A[Start]
-    B{Choose Sport}
-    C{Choose Level}
-    D{Choose Gender}
-    E{Choose League}
-    F[Load Overtime Rules]
-    G[Display Rules]
-    H[End]
-    A --> B
-    B -->|One of the major 4| C
-    B -->|Other| E
-    C --> D
-    D --> E
-    E --> F
-    F --> G
-    G --> H
-    
+    A[Start] --> B[Fetch Overtime Rules from API]
+    B --> C[Store Overtime Rules in Local Storage]
+    C --> D[Display Form for User Input]
+    D --> E[Get User Inputs: Sport, Level, League, Season Type]
+    E --> F[Validate Sport Input]
+    F --> G{Is Sport Input Valid?}
+    G -- Yes --> H[Fetch Appropriate Rules from Storage]
+    G -- No --> I[Prompt User for Correct Input]
+    H --> J[Differentiation by Season Type]
+    J --> K[Display Overtime Rules in Web App]
+    K --> L[Get Timeouts and Challenges Info]
+    L --> M[Display Timeouts and Challenges Information]
+    M --> N{Start Countdown Clock?}
+    N -- Yes --> O[Start Real-time Countdown Clock]
+    N -- No --> P[End]
+    O --> P[End]
